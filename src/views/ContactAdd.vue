36ac3e161739d0cd0b368d93c0bb1faeb73d0490
MaNguyenMinhTri <trib2111869@student.ctu.edu.vn>
2024-10-31 09:55:30 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page">
+        <h4>Tạo Liên hệ mới</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "../components/ContactForm.vue";
+import ContactService from "../services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ mới đã được tạo thành công.";
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
